41537b03f9af51f225eb1962abc771bb9956e189
ndahiro james <mukunzindahiro@gmail.com>
2024-01-07 11:59:43 -0800

    changes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const inputValues =Array.from(document.querySelectorAll("input")).map(input=>
-    
-    input.value
-    )
-const inputs =Array.from(document.querySelectorAll("input"))
-inputs.forEach((onclick)=>{
+const inputs = document.querySelectorAll("input");
+let inputsArray = Array.from(inputs);
+let inputValues = inputsArray.map((input) => input.value);
 
-})
+const grantLoginAccess = () => {
+  document.getElementById("login").style.backgroundColor = "yellow";
+  document.getElementById("login").style.color = "black";
+};
+
+const denyLoginAccess = () => {
+  document.getElementById("login").style.backgroundColor = "grey";
+  document.getElementById("login").style.color = "white";
+};
+
+inputs.forEach((input) =>
+  input.addEventListener("change", () => {
+    inputsArray = Array.from(inputs);
+    inputValues = inputsArray.map((input) => input.value);
+    const bothInputsHaveContent = inputValues.every((value) => value);
+    bothInputsHaveContent ? grantLoginAccess() : denyLoginAccess();
+  })
+);
